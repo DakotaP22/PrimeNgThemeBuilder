e6de45bac3113d8a0b77b959b76bbfe418dcdfca
Dakota Pease <peased22@gmail.com>
2024-09-20 02:29:25 +0000

    feat: Adds menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { MenuItem, PrimeIcons } from "primeng/api";
+import { ButtonModule } from "primeng/button";
+import { MenubarModule } from "primeng/menubar";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ButtonModule, MenubarModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'PrimeNgThemeBuilder';
+  title = "PrimeNgThemeBuilder";
+
+  menuItems: MenuItem[] = [
+    {
+      label: "Browse Prebuilt Themes",
+      icon: PrimeIcons.SEARCH,
+    },
+    {
+      label: "Start From Scratch",
+      icon: PrimeIcons.FILE_EDIT,
+    },
+  ];
 }
